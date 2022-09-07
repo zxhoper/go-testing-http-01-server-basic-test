3150392d32bdf88c2da440cedee71fe096d52121
Zxander Hoper <zxhoper@proton.me>
2022-09-07 16:38:23 +0800

    test for handler OK

--- main_test.go
+++ main_test.go
@@ -59,45 +59,48 @@ func TestDoubleHandlerWithTableOfTestCases(t *testing.T) {
 	}{
 		{name: "double of two", value: "2", double: 4},
 		{name: "missing value", value: "", err: "missing value"},
+		{name: "not a number", value: "x", err: "not a number: x"},
 	}
 
 	for _, tc := range tt {
-		req, err := http.NewRequest("GET", "localhost:8080/double?v="+tc.value, nil)
-		if err != nil {
-			t.Fatalf("could not create request: %v", err)
-		}
-		rec := httptest.NewRecorder()
-		doubleHandler(rec, req)
-
-		res := rec.Result()
-		defer res.Body.Close()
-
-		b, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			t.Fatalf("could not read response: %v", err)
-		}
-
-		if tc.err != "" {
-			// do something
-			if res.StatusCode != http.StatusBadRequest {
-				t.Errorf("expected status Bad Request; got %v", res.StatusCode)
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", "localhost:8080/double?v="+tc.value, nil)
+			if err != nil {
+				t.Fatalf("could not create request: %v", err)
 			}
-			if msg := string(bytes.TrimSpace(b)); msg != tc.err {
-				t.Errorf("expected message %q; got %q", tc.err, msg)
+			rec := httptest.NewRecorder()
+			doubleHandler(rec, req)
+
+			res := rec.Result()
+			defer res.Body.Close()
+
+			b, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Fatalf("could not read response: %v", err)
 			}
-			return
-		}
 
-		if res.StatusCode != http.StatusOK {
-			t.Errorf("expected status OK; got %v", res.Status)
-		}
+			if tc.err != "" {
+				// do something
+				if res.StatusCode != http.StatusBadRequest {
+					t.Errorf("expected status Bad Request; got %v", res.StatusCode)
+				}
+				if msg := string(bytes.TrimSpace(b)); msg != tc.err {
+					t.Errorf("expected message %q; got %q", tc.err, msg)
+				}
+				return
+			}
 
-		d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
-		if err != nil {
-			t.Fatalf("expected an interger; got %s", b)
-		}
-		if d != tc.double {
-			t.Fatalf("expectged double to be %v; got %v", tc.double, d)
-		}
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("expected status OK; got %v", res.Status)
+			}
+
+			d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
+			if err != nil {
+				t.Fatalf("expected an interger; got %s", b)
+			}
+			if d != tc.double {
+				t.Fatalf("expectged double to be %v; got %v", tc.double, d)
+			}
+		})
 	}
 }
