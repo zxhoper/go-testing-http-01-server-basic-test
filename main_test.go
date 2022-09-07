73e707b7e6bb8c6ebba4e151919c390d41bdc360
Zxander Hoper <zxhoper@proton.me>
2022-09-07 16:10:42 +0800

    finish one TC(test case) in Testing Table

--- main_test.go
+++ main_test.go
@@ -49,3 +49,48 @@ func TestDoubleHandler(t *testing.T) {
 		t.Fatalf("expectged double to be 4; got %v", d)
 	}
 }
+
+func TestDoubleHandlerWithTableOfTestCases(t *testing.T) {
+	tt := []struct {
+		name   string
+		value  string
+		double int
+		err    string
+	}{
+		{name: "double of two", double: 4, value: "2"},
+	}
+
+	for _, tc := range tt {
+		req, err := http.NewRequest("GET", "localhost:8080/double?v="+tc.value, nil)
+		if err != nil {
+			t.Fatalf("could not create request: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		doubleHandler(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		if tc.err != "" {
+			// do something
+			return
+		}
+
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("expected status OK; got %v", res.Status)
+		}
+
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatalf("could not read response: %v", err)
+		}
+
+		d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
+		if err != nil {
+			t.Fatalf("expected an interger; got %s", b)
+		}
+		if d != tc.double {
+			t.Fatalf("expectged double to be %v; got %v", tc.double, d)
+		}
+	}
+}
